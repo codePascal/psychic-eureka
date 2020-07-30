45c9e3c47d94761787dc4ca4345109f1098b6946
PMU <pascal.mueller@thurweb.ch>
2020-07-30 19:01:48 +0200

    New files added

---
+++ project_euler_246.py
@@ -0,0 +1,7 @@
+"""
+Definition of an ellipse:
+Given circle c with center M(x1,y) and radius r;
+Given a point G(x2,y) inside circle c;
+Ellipse e is formed with points p such that d(p,G) == d(p,c);
+
+"""
