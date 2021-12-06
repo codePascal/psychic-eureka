8b7bfb52fc24c22a4b1356343e6519e88795cae6
PMU <pascal.mueller@thurweb.ch>
2021-12-06 14:15:36 +0100

    added description to the shell script that runs the problems.

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
+#
+##############################################################################
+# run
+#
+# Execute this shell script inside the repository. The argument denotes the
+# project number, that will be solved. If a problem is not implemented yet, a
+# 'NotImplementedError' will arise.
+#
+# Usage example:
+# bash run.sh 1
+#
+###############################################################################
 
 python3 projects/project_euler_$1.py
